baa7cae156b97edea1e305795b6c8dec096b51bb
Filip Hrenić <hrenic.filip@gmail.com>
2016-04-18 09:51:10 +0200

    forgot to change index.php

--- index.php
+++ index.php
@@ -5,11 +5,9 @@ require_once "src/Util/init.php";
 try {
     \Dispatch\Dispatcher::getInstance()->dispatch();
 } catch (\Routing\RoutingException $e) {
-    var_dump($e);
-//    redirect(\Routing\Route::get('error')->generate(['code' => '404']));
+    redirect(\Routing\Route::get('error')->generate(['code' => '404']));
 } catch (Exception $e) {
-    var_dump($e);
-//    redirect(\Routing\Route::get('error')->generate(['code' => '500']));
+    redirect(\Routing\Route::get('error')->generate(['code' => '500']));
 }
 
 
